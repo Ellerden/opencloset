a6f32b2f32d07cae8053b4940665ad4dec823ded
Daria Shcheglova <ellerden@gmail.com>
2019-10-24 10:53:42 -0700

    wait for ajax added

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include FeatureHelpers, type: :feature
+  config.include WaitForAjax, type: :feature
   config.include OmniauthMacros
 
   Capybara.javascript_driver = :selenium_chrome
